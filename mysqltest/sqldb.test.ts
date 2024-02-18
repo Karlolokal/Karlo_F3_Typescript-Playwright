307a7ac72a4414f17b430d79375824e52792f2e3
karlo.cunanan <karlomiguel.cunanan@asurion.com>
2024-02-18 09:13:31 +0800

    2nd commit

--- mysqltest/sqldb.test.ts
+++ mysqltest/sqldb.test.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 async function run() {
     const connection = await mysql.createConnection({
-        host: 'backoffice-iglobis-qa-db-usa.backoffice.npr.aws.asurion.net',
-        user: 'svc.US-q-IGLOBIS',
-        password: 'TAM7NU5R',
-        database: 'iglobis',
+        host: 'localhost',
+        user: '',
+        password: '',
+        database: '',
         port: 3330
 });
     const query = await connection.execute('Select * from Invoice limit 10;');
